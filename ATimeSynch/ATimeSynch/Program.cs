3bdeadc50e183d0bba8881b0ebbaf502b36f8c1c
Abhimanbhau Kolte <abhimanbhau@gmail.com>
2014-10-31 15:45:20 +0530

    Init

---
+++ ATimeSynch/ATimeSynch/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+
+namespace ATimeSynch
+{
+    class Program
+    {
+        public struct SYSTEMTIME
+        {
+            public ushort Year;
+            public ushort Month;
+            public ushort DayOfWeek;
+            public ushort Day;
+            public ushort Hour;
+            public ushort Minute;
+            public ushort Second;
+            public ushort Milliseconds;
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetSystemTime", SetLastError = true)]
+        public extern static bool Win32SetSystemTime(ref SYSTEMTIME sysTime);
+        static void Main(string[] args)
+        {
+            if (!UacHelper.IsProcessElevated)
+            {
+                Console.WriteLine("Restart with admin privilages");
+                return;
+            }
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                DateTime ntpTime = Util.GetNetworkTime();
+                var diff = DateTime.Now.Subtract(ntpTime.ToLocalTime()).TotalMilliseconds;
+                Console.WriteLine("Time Drift(ms) -> " + diff);
+                Console.WriteLine("System Time -> " + DateTime.Now + DateTime.Now.Millisecond);
+                Console.WriteLine("Atomic Time -> " + ntpTime.ToLocalTime() + ntpTime.ToLocalTime().Millisecond);
+                sw.Stop();
+                var timeToSet = new SYSTEMTIME
+                {
+                    Year = (ushort)ntpTime.Year,
+                    Month = (ushort)ntpTime.Month,
+                    DayOfWeek = (ushort)ntpTime.DayOfWeek,
+                    Day = (ushort)ntpTime.Day,
+                    Hour = (ushort)ntpTime.Hour,
+                    Minute = (ushort)ntpTime.Minute,
+                    Second = (ushort)ntpTime.Second,
+                    Milliseconds = (ushort)(ntpTime.Millisecond+sw.ElapsedMilliseconds)
+                };
+                Win32SetSystemTime(ref timeToSet);
+                Console.WriteLine("Time sync with NTP Server successfull");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ ATimeSynch/ATimeSynch/Util.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ATimeSynch
+{
+    class Util
+    {
+        public static DateTime GetNetworkTime()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            const string ntpServer = "3.asia.pool.ntp.org";
+            var ntpData = new byte[48];
+            ntpData[0] = 0x1B;
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Connect(ipEndPoint);
+            socket.ReceiveTimeout = 3000;
+            socket.Send(ntpData);
+            socket.Receive(ntpData);
+            socket.Close();
+            const byte serverReplyTime = 40;
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+            sw.Stop();
+            milliseconds += Convert.ToUInt64(sw.ElapsedMilliseconds);
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+            return networkDateTime;
+        }
+        static uint SwapEndianness(ulong x)
+        {
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
+        }
+    }
+}
